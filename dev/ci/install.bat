bdaa903da833decc577f40e01419ed81a043e486
Zakariyya Mughal <zaki.mughal@gmail.com>
2016-06-29 06:38:45 -0500

    Appveyor: deployment to BinTray

---
+++ dev/ci/install.bat
@@ -0,0 +1,23 @@
+@echo off
+
+cd %APPVEYOR_BUILD_FOLDER%
+
+echo Compiler: %COMPILER%
+echo Architecture: %MSYS2_ARCH%
+echo Platform: %PLATFORM%
+echo MSYS2 directory: %MSYS2_DIR%
+echo MSYS2 system: %MSYSTEM%
+echo Bits: %BIT%
+
+REM Create a writeable TMPDIR
+mkdir %APPVEYOR_BUILD_FOLDER%\tmp
+set TMPDIR=%APPVEYOR_BUILD_FOLDER%\tmp
+
+IF %COMPILER%==msys2 (
+  @echo on
+  SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
+  bash -lc "cd $APPVEYOR_BUILD_FOLDER; ./dev/script/get-aux-repo"
+  bash -lc "cd $APPVEYOR_BUILD_FOLDER; ./project-renard/curie/dev/ci/appveyor/update-msys2.sh"
+  bash -lc "cd $APPVEYOR_BUILD_FOLDER; APPVEYOR_BUILD_FOLDER=$APPVEYOR_BUILD_FOLDER/project-renard/curie ./project-renard/curie/dev/script/install-native-dep"
+  bash -lc "cd $APPVEYOR_BUILD_FOLDER; ./dev/ci/par-packer-prefix.sh $APPVEYOR_BUILD_FOLDER/build/project-renard-$APPVEYOR_BUILD_VERSION"
+)
