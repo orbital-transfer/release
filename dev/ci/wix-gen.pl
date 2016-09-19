71152056435f3f7a60cba91346a841ce4df91240
Zakariyya Mughal <zaki.mughal@gmail.com>
2016-09-18 20:04:23 -0500

    WiX ProductId should remain the same between runs

--- dev/ci/wix-gen.pl
+++ dev/ci/wix-gen.pl
@@ -1,5 +1,33 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
 use Template;
 use Data::UUID;
+use FindBin;
+use YAML qw(DumpFile LoadFile);
+
+my $config_file = File::Spec->catfile(
+	$FindBin::Bin,
+	'config.yml' );
+
+my $config;
+if( @ARGV > 0 && $ARGV[0] eq '--config' ) {
+	$config = {
+		product_uuid => Data::UUID->new->create_str,
+	};
+	DumpFile( $config_file, $config );
+}
+
+if( ! -f $config_file ) {
+	die <<DIE;
+Create config file and commit to repository by running:
+    $0 --config
+DIE
+} else {
+	$config = LoadFile( $config_file );
+}
 
 my $data = {
 	manufacturer => 'Project Renard',
@@ -8,6 +36,7 @@ my $data = {
 	package_comments => 'Project Renard is an FOSS project',
 	package_version => '0.001',
 	uuid => Data::UUID->new,
+	%{$config},
 };
 
 my $tt = Template->new;
@@ -17,7 +46,7 @@ $tt->process( \*DATA, $data, "" ) or die $tt->error, "\n";
 __DATA__
 <?xml version='1.0' encoding='windows-1252'?>
 <Wix xmlns='http://schemas.microsoft.com/wix/2006/wi'>
-  <Product Name='[% product_name %]' Id='[% uuid.create_str() %]' UpgradeCode='[% uuid.create_str() %]'
+  <Product Name='[% product_name %]' Id='[% product_uuid %]' UpgradeCode='[% uuid.create_str() %]'
     Language='1033' Codepage='1252' Version='[% package_version %]' Manufacturer='[% manufacturer %]'>
 
     <Package Id='*' Keywords='Installer' Description="[% package_description %]"
